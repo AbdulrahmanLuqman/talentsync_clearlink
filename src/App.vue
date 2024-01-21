7b0849024599fe0821341f627078d766b6190be9
AbdulrahmanLuqman <fal528@dervac.com>
2024-01-21 18:33:44 +0100

    done with first section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script>
+import { defineComponent } from 'vue';
+import Navbar from './components/Navbar.vue';
+import BackgroundPattern from './components/BackgroundPattern.vue'
+import FirstSection from './components/FirstSection.vue'
+
+export default defineComponent({
+    components:{
+        Navbar, BackgroundPattern, FirstSection
+    }
+})
 </script>
 
 <template>
+    <main class="pt-[40px] px-[100px] pb-[96px] space-y-[120px]">
+        <Navbar class="z-[999] relative"/>
+        <BackgroundPattern class="absolute top-0 left-0 z-[-10]"/>
+        <FirstSection />
+    </main>
 </template>
 
 <style scoped>
---
+++ src/components/Companies.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <p>Join 1,500+ companies already video conferencing the ClearLink way</p>
+        <div>
+            <img src="../assets/images/" alt="">
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
